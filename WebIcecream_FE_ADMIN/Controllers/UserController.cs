e0d8caf4326f9ffa1c1edd98432b686d448f43bf
Anhle138795 <ank138795@gmail.com>
2024-05-27 15:10:07 +0700

    add crud user admin

---
+++ WebIcecream_FE_ADMIN/Controllers/UserController.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using WebIcecream_FE_ADMIN.Models;
+using X.PagedList;
+
+namespace WebIcecream_FE_ADMIN.Controllers
+{
+    public class UserController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:7018/api");
+        private readonly HttpClient _client;
+
+        public UserController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public IActionResult Index(int? page)
+        {
+            List<UserViewModel> list = new List<UserViewModel>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/User/GetUsers").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                list = JsonConvert.DeserializeObject<List<UserViewModel>>(data);
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            return View(list.ToPagedList(pageNumber, pageSize));
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserViewModel model)
+        {
+            try
+            {
+                string data = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = _client.PostAsync(_client.BaseAddress + "/User/PostUser", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["successMessage"] = "User has been successfully created.";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            try
+            {
+                UserViewModel model = new UserViewModel();
+                HttpResponseMessage response = _client.GetAsync($"{_client.BaseAddress}/User/GetUser/{id}").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = response.Content.ReadAsStringAsync().Result;
+                    model = JsonConvert.DeserializeObject<UserViewModel>(data);
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, UserViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                string data = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = _client.PutAsync($"{_client.BaseAddress}/User/PutUser", content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["successMessage"] = "User has been updated.";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = "An error occurred while updating the user.";
+                    return View(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                HttpResponseMessage response = _client.GetAsync($"{_client.BaseAddress}/User/GetUser/{id}").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = response.Content.ReadAsStringAsync().Result;
+                    UserViewModel model = JsonConvert.DeserializeObject<UserViewModel>(data);
+                    return View(model);
+                }
+                else
+                {
+                    TempData["errorMessage"] = "User not found for deletion.";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                HttpResponseMessage response = _client.DeleteAsync($"{_client.BaseAddress}/User/DeleteUser/{id}").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["successMessage"] = "User has been deleted.";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = "An error occurred while deleting the user.";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
