63466e0f8eda269d292d652fde6cd60f80f239b3
tranlocccc123 <ttranloc2004@gmail.com>
2024-05-28 21:17:37 +0700

    login, logout, Tạo trang sản phẩm

--- WebIcecream_FE/Controllers/ProductController.cs
+++ WebIcecream_FE/Controllers/ProductController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebIcecream_FE.Models;
 
 namespace WebIcecream_FE.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            using (HttpClient client = new HttpClient())
+            {
+                string apiUrl = "https://localhost:7018/api/Books/GetBooks";
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+
+                    List<Product> productList = JsonConvert.DeserializeObject<List<Product>>(responseData); 
+
+                    //ViewBag.ApiData = responseData;
+                    return View(productList);
+
+                }
+                else
+                {
+                    //ViewBag.ApiData = "Error calling the API";
+                    return View(new List<Product>());
+
+                }
+            }
         }
     }
 }
---
+++ WebIcecream_FE/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebIcecream_FE.Models
+{
+    public class Product
+    {
+        public int bookId;
+        public string title;
+        public string description;
+        public string imageUrl;
+        public double price;
+
+    }
+}
