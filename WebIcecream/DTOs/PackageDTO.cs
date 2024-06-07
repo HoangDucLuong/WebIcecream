e13a850817ea050799fb9f6f81797df3ed5e3330
tranlocccc123 <ttranloc2004@gmail.com>
2024-06-07 13:01:12 +0700

    sửa

---
+++ WebIcecream/DTOs/PackageDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WebIcecream.DTOs
+{
+    public class PackageDTO
+    {
+        public int PackageID { get; set; }
+        public string PackageName { get; set; }
+        public double Price { get; set; }
+        public int DurationDays { get; set; }
+    }
+}
---
+++ WebIcecream/Models/Package.cs
@@ -0,0 +1,12 @@
+﻿namespace WebIcecream.Models
+{
+    public class Package
+    {
+        public int PackageID { get; set; }
+        public string PackageName { get; set; } = null!;
+        public double Price { get; set; }
+        public int DurationDays { get; set; }
+
+
+    }
+}
